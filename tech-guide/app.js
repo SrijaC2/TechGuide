08c8904a5931ac0e6715b2157616d32bd21c68a2
srija <srija@DESKTOP-BN7HD8R>
2023-07-21 20:37:32 +0530

    The Tech Guide

--- tech-guide/app.js
+++ tech-guide/app.js
@@ -1,39 +1,32 @@
 const { request, response } = require("express");
 const express = require("express");
-// const csrf = require("tiny-csrf");
 const app = express();
-// const { Persons,Rooms,Events } = require("./models");
-// const bodyParser = require("body-parser");
-// const cookieParser = require("cookie-parser");
-// app.use(bodyParser.json());
-
 app.set("view engine", "ejs");
 app.engine('ejs', require('ejs').__express);
 const path = require("path");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.static(__dirname + '/public'));
-
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser("shh! some secret string"));
-// app.use(csrf("this_should_be_32_character_long", ["POST", "PUT", "DELETE"]));
-
-// const passport = require("passport");
-// const connectEnsureLogin = require("connect-ensure-login");
-// const session = require("express-session");
-// const LocalStrategy = require("passport-local");
-
-// const flash = require("connect-flash");
 app.set("views", path.join(__dirname, "views"));
+
+let BucketList = [
+  { url: "https://www.youtube.com/embed/vhpC4m61w-o", type: 'Shop' ,name:'Amazon', try: "https://www.amazon.in/"},
+  { url: "https://www.youtube.com/embed/orOdajUOC6o", type: 'Food' ,name:'Swiggy', try: "https://www.swiggy.com/"},
+  { url: "https://www.youtube.com/embed/-E8IQ8jekkQ", type: 'Payment' ,name:'Google Pay', try: "https://pay.google.com/about/"},
+  { url: "https://www.youtube.com/embed/D8HI7Bg4Dqw", type: 'Travel' ,name:'Uber', try: "https://m.uber.com/"}
+];
+
 app.get("/", async (request, response) => {
   return response.render("index");
 });
-
 app.get("/typesOfApps", async (request, response) => {
   return response.render("typesOfApps");
 });
 
 app.get("/video/:id", async (request, response) => {
-  return response.render("video");
+  let id = request.params.id;
+  return response.render("video",{
+    object: BucketList[id]
+  });
 });
 
 
